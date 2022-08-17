229bee26831e81342def758d247567fda9f5672b
dffleh <dffleh1995@gmail.com>
2022-08-17 21:54:17 +0300

    add hw

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryList = document.querySelector('.js-gallery');
+const itemsMarkup = galleryItems.map(({preview, original, description}) => {
+    return `
+    <a class="gallery__link" href="${original}">
+    <img 
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        />
+    </a>
+    `
+}).join('');
+
+galleryList.insertAdjacentHTML('afterbegin', itemsMarkup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
